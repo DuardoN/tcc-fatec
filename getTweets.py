e34a977805736d0bcb6fed32d979af3adfc33974
Eduardo Neves <neves98@outlook.com>
2019-05-18 15:26:27 -0300

    alter getTweets with words

--- getTweets.py
+++ getTweets.py
@@ -19,21 +19,50 @@ auth = OAuth1 (
     auth_params['oauth_token_secret']
 )
 
+# Words about negative/positive for query
+words = [
+    'bad',
+    'triste',
+    'tristeza',
+    'repudio',
+    ':(',
+    'sad',
+    'caralho',
+    'depressão',
+    'decepcionado',
+    'decepcionada',
+    'insatisfeito',
+    'ódio',
+    'cansada',
+    'exausta',
+    'cansado',
+    'exausto',
+    'feliz',
+    'alegre',
+    'sorrindo',
+    'apaixonada',
+    'apaixonado',
+    'paz',
+    ':)',
+    'felicidade',
+    'união',
+    'top',
+    'topzera',
+]
+
 # url according to twitter API
 url_rest = "https://api.twitter.com/1.1/search/tweets.json"
 
-# query used by get tweets
-q = ':('
+for word in words:
+    # count : no of tweets to be retrieved per one call and parameters according to twitter API
+    params = {'q': word, 'count': 100, 'lang': 'pt',  'result_type': 'mixed'}
+    results = requests.get(url_rest, params=params, auth=auth)
+    tweets = results.json()
 
-# count : no of tweets to be retrieved per one call and parameters according to twitter API
-params = {'q': q, 'count': 100, 'lang': 'pt',  'result_type': 'mixed'}
-results = requests.get(url_rest, params=params, auth=auth)
-tweets = results.json()
+    # open csv for save tweets
+    csvFile = open('tweets-'+word+'.csv', 'w')
+    csvWriter = csv.writer(csvFile)
 
-# open csv for save tweets
-csvFile = open('tweets.csv', 'w')
-csvWriter = csv.writer(csvFile)
-
-for tweet in tweets['statuses']:
-    # Write on csv
-    csvWriter.writerow([tweet['text'].encode('utf-8')])
+    for tweet in tweets['statuses']:
+        # Write on csv
+        csvWriter.writerow([tweet['text'].encode('utf-8')])
